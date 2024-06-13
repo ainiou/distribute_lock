4373786d3b435143479277e425334af13fb937cb
xiawenhao <xiawenhao@zonst.cn>
2024-06-13 20:29:45 +0800

    lockval

--- lock.go
+++ lock.go
@@ -54,19 +54,19 @@ func WithBackoffList(backoffList []time.Duration) Option {
 	}
 }
 
-func (d *DistributeLock) ObtainLock(lockKey string, ex int) (bool, error) {
+func (d *DistributeLock) ObtainLock(lockKey, lockVal string, ex int) (bool, error) {
 	if d.conn == nil {
 		return false, errors.New("连接为空")
 	}
 	// 尝试获取
-	getLock, _ := d.lock(lockKey, ex)
+	getLock, _ := d.lock(lockKey, lockVal, ex)
 	if getLock {
 		return true, nil
 	}
 
 	var err error
 	err = retry.RetryFunc(func() error {
-		lock, _ := d.lock(lockKey, ex)
+		lock, _ := d.lock(lockKey, lockVal, ex)
 		if lock {
 			return nil
 		}
@@ -78,9 +78,9 @@ func (d *DistributeLock) ObtainLock(lockKey string, ex int) (bool, error) {
 	return false, err
 }
 
-func (d *DistributeLock) lock(lockKey string, ex int) (bool, error) {
+func (d *DistributeLock) lock(lockKey string, lockVal string, ex int) (bool, error) {
 	// 尝试获取
-	reply, err := redis.String(d.conn.Do("SET", lockKey, "done", "NX", "EX", ex))
+	reply, err := redis.String(d.conn.Do("SET", lockKey, lockVal, "NX", "EX", ex))
 	// 测试发现存在也不会有错误，而是 reply = nil
 	if err != nil && !errors.Is(err, redis.ErrNil) {
 		return false, err
@@ -93,13 +93,18 @@ func (d *DistributeLock) lock(lockKey string, ex int) (bool, error) {
 	return false, errors.New(fmt.Sprintf("未获取到锁:%s", err.Error()))
 }
 
-func (d *DistributeLock) ReleaseLock(lockKey string) error {
+func (d *DistributeLock) ReleaseLock(lockKey, lockVal string) error {
 	if d.conn == nil {
 		return errors.New("连接为空")
 	}
-	_, err := d.conn.Do("EVAL", ReleaseScript, 1, lockKey, "done")
+	reply, err := d.conn.Do("EVAL", ReleaseScript, 1, lockKey, lockVal)
 	if err != nil {
 		return err
+	}
+	// 这下面的逻辑也可以不处理。发生如下逻辑说明发生了激烈的锁竞争 即上一个线程的处理时间超过了锁的过期时间(EX) key 自动过期且其他线程获取到锁。
+	// 这时第一个线程释放锁时 get and compare 失败，del 删除0个key
+	if reply.(int64) != 1 {
+		return errors.New(fmt.Sprintf("释放锁失败:key:%s,val:%s", lockKey, lockVal))
 	}
 	return nil
 }
--- lock_test.go
+++ lock_test.go
@@ -1,10 +1,10 @@
 package distribute_lock
 
 import (
+	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"testing"
 	"time"
-	"zonst/qipai/api/clubbaseinfoapisrv/utils"
 )
 
 var pool *redis.Pool
@@ -12,7 +12,7 @@ var DL *DistributeLock
 
 func before() {
 
-	pool = utils.NewPool("127.0.0.1:6379", "root", 0)
+	pool = newPool("127.0.0.1:6379", "root", 0)
 	conn := pool.Get()
 	//defer conn.Close()
 	DL = NewDistributeLock(conn,
@@ -20,19 +20,57 @@ func before() {
 		WithBackoffList([]time.Duration{time.Millisecond * 50, time.Millisecond * 500, time.Second * 2}),
 	)
 }
+
+func newPool(addr, password string, db int) *redis.Pool {
+
+	pl := &redis.Pool{
+		MaxIdle:     600,
+		MaxActive:   600,
+		IdleTimeout: 300 * time.Second,
+		Dial: func() (redis.Conn, error) {
+
+			c, err := redis.Dial("tcp", addr,
+				redis.DialDatabase(db),
+				redis.DialPassword(password),
+				redis.DialConnectTimeout(500*time.Millisecond),
+				redis.DialReadTimeout(500*time.Millisecond),
+				redis.DialWriteTimeout(500*time.Millisecond),
+			)
+			if err != nil {
+				panic(err)
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+
+	return pl
+}
 func TestDistributeLock_ObtainLock(t *testing.T) {
 	before()
-	obtainLock, err := DL.ObtainLock("lockTest", 1000)
+	lockVal := fmt.Sprintf("%d", time.Now().Nanosecond())
+	obtainLock, err := DL.ObtainLock("lockTest", lockVal, 1000)
 	if err != nil {
 		t.Errorf("err:%v", err)
 		return
 	}
 	t.Logf("botain:%v", obtainLock)
+
+	time.Sleep(15 * time.Second)
+	err = DL.ReleaseLock("lockTest", lockVal)
+	if err != nil {
+		t.Errorf("err:%v", err)
+		return
+	}
+	t.Logf("release")
 }
 
 func TestDistributeLock_ReleaseLock(t *testing.T) {
 	before()
-	err := DL.ReleaseLock("lockTest")
+	err := DL.ReleaseLock("lockTest", "done")
 	if err != nil {
 		t.Errorf("err:%v", err)
 		return
