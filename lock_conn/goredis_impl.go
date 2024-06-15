374658a07f6001cea042c140f906ef64f64c36fc
xiawenhao <xiawenhao@zonst.cn>
2024-06-15 15:32:40 +0800

    goredis 客户端简单实现

--- lock_conn/goredis_impl.go
+++ lock_conn/goredis_impl.go
@@ -1,1 +1,42 @@
 package lock_conn
+
+import (
+	"context"
+	"errors"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+type GoRedisConn struct {
+	conn *redis.Client
+	ctx  context.Context
+}
+
+func NewGoRedisConn(ctx context.Context, conn *redis.Client) *GoRedisConn {
+	return &GoRedisConn{conn: conn, ctx: ctx}
+}
+
+func (d *GoRedisConn) ObtainLock(lockKey, lockVal string, ex int) (bool, error) {
+	if d.conn == nil {
+		return false, errors.New("conn is nil")
+	}
+	ok, err := d.conn.SetNX(d.ctx, lockKey, lockVal, time.Second*time.Duration(ex)).Result()
+	if err != nil {
+		return false, err
+	}
+	if !ok {
+		return false, errors.New("未获取到锁")
+	}
+	return true, nil
+}
+
+func (d *GoRedisConn) ReleaseLock(lockKey, lockVal string) error {
+	if d.conn == nil {
+		return errors.New("conn is nil")
+	}
+	_, err := d.conn.Eval(d.ctx, ReleaseScript, []string{lockKey}, lockVal).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- lock_test.go
+++ lock_test.go
@@ -1,23 +1,33 @@
 package distribute_lock
 
 import (
+	"context"
 	"fmt"
 	"github.com/ainiou/distribute_lock/lock_conn"
 	"github.com/garyburd/redigo/redis"
+	goredis "github.com/go-redis/redis/v8"
 	"testing"
 	"time"
 )
 
 var pool *redis.Pool
 var DL *DistributeLock
+var ctx = context.Background()
 
 func before() {
 
-	pool = newPool("127.0.0.1:6379", "root", 0)
-	conn := pool.Get()
-	lockConn := lock_conn.NewRedigoConn(conn)
-	//defer conn.Close()
-	DL = NewDistributeLock(lockConn,
+	//pool = newPool("127.0.0.1:6379", "root", 0)
+	//conn := pool.Get()
+	//lockConn := lock_conn.NewRedigoConn(conn)
+	////defer conn.Close()
+	//DL = NewDistributeLock(lockConn,
+	//	WithMaxRetryTime(3),
+	//	WithBackoffList([]time.Duration{time.Millisecond * 50, time.Millisecond * 500, time.Second * 2}),
+	//)
+
+	client := newClient("127.0.0.1:6379", "root", 0)
+	redisConn := lock_conn.NewGoRedisConn(ctx, client)
+	DL = NewDistributeLock(redisConn,
 		WithMaxRetryTime(3),
 		WithBackoffList([]time.Duration{time.Millisecond * 50, time.Millisecond * 500, time.Second * 2}),
 	)
@@ -51,6 +61,20 @@ func newPool(addr, password string, db int) *redis.Pool {
 
 	return pl
 }
+
+func newClient(addr, password string, db int) *goredis.Client {
+	client := goredis.NewClient(&goredis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+	reply, err := client.Ping(ctx).Result()
+	if err != nil || reply != "PONG" {
+		panic(err)
+	}
+	return client
+}
+
 func TestDistributeLock_ObtainLock(t *testing.T) {
 	before()
 	lockVal := fmt.Sprintf("%d", time.Now().Nanosecond())
